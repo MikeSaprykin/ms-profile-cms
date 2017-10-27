5e01c053acedd88381037dbad4d22d9f778db3bc
mikesaprykin <mikesaprykin@gmail.com>
2017-10-27 09:22:27 +0300

    feat(react-apollo): Connected root component to react-apollo and store

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,17 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { ApolloProvider, ApolloClient, createNetworkInterface } from 'react-apollo';
+
+const uri = 'http://localhost:8080/graphql';
+const networkInterface = createNetworkInterface({ uri });
+
+const client = new ApolloClient({ networkInterface, connectToDevTools: true });
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+      <App />
+  </ApolloProvider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
