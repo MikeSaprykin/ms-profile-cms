734af1d8aa965bedabff899acbdec6b5e4822233
mikesaprykin <mikesaprykin@gmail.com>
2017-10-31 18:41:09 +0200

    feat(todo-header, root, mutations): Added functional to add todo

--- src/components/todo-header/todo-header.tsx
+++ src/components/todo-header/todo-header.tsx
@@ -29,34 +29,69 @@ const filterItems = [
     }
 ];
 
-const todoHeader: React.StatelessComponent<TodoHeaderProps> = ({ onAddClick, onTodoFilter, currentFilter }) => {
-    const generateFilterItems = () => filterItems.map(item =>
-          (
-              <TodoFilterItem
-                  key={item.value}
-                  onClick={() => onTodoFilter(item.value)}
-                  active={currentFilter === item.value}
-              >
-                  {item.title}
-              </TodoFilterItem>
-          )
-      );
-    return (
-        <TodoHeader>
-            <TodoHeaderPlus onClick={() => onAddClick()}>
-                <i className="material-icons">add_box</i>
-            </TodoHeaderPlus>
-            <TodoHeaderText>
-                <TodoHeaderInput
-                    onChange={(e: any) => console.log(e)}
-                    placeholder="Add todo..."
-                />
-            </TodoHeaderText>
-            <TodoFilterWrapper>
-                {generateFilterItems()}
-            </TodoFilterWrapper>
-        </TodoHeader>
-    );
-};
+class TodoHeaderComponent extends React.Component<TodoHeaderProps, any> {
 
-export default todoHeader;
+    textInput: any;
+
+    constructor(props: TodoHeaderProps) {
+        super(props);
+        this.state = { value: '' };
+    }
+
+    render() {
+        const { onAddClick, onTodoFilter, currentFilter } = this.props;
+        const generateFilterItems = () => filterItems.map(item =>
+            (
+                <TodoFilterItem
+                    key={item.value}
+                    onClick={() => onTodoFilter(item.value)}
+                    active={currentFilter === item.value}
+                >
+                    {item.title}
+                </TodoFilterItem>
+            )
+        );
+        const handleChange = (event: any) => {
+            this.setState({value: event.target.value});
+        };
+        const handleKeyDown = (event: KeyboardEvent) => {
+          if (event.keyCode === 13) {
+              handleSubmit();
+          }
+        };
+        const handleSubmit = () => {
+            const { value } = this.state;
+            if (value) {
+                onAddClick({value});
+                this.setState({value: ''});
+                if (this.textInput) {
+                    this.textInput.blur();
+                }
+            }
+        };
+        const onInputSubmit = () => {
+          handleSubmit();
+        };
+        return (
+            <TodoHeader>
+                <TodoHeaderPlus onClick={() => onInputSubmit()}>
+                    <i className="material-icons">add_box</i>
+                </TodoHeaderPlus>
+                <TodoHeaderText>
+                    <TodoHeaderInput
+                        innerRef={(comp: any) => this.textInput = comp}
+                        value={this.state.value}
+                        onChange={handleChange}
+                        onKeyDown={handleKeyDown}
+                        placeholder="Add todo..."
+                    />
+                </TodoHeaderText>
+                <TodoFilterWrapper>
+                    {generateFilterItems()}
+                </TodoFilterWrapper>
+            </TodoHeader>
+        );
+    }
+}
+
+export default TodoHeaderComponent;
